f30bf5a2a99919485b248f679fad46cf48ec65f3
DanielSanchezMejia <33704938+DanielSanchezMejia@users.noreply.github.com>
2017-11-17 18:08:06 -0500

    cuenta007

--- app.js
+++ app.js
@@ -301,10 +301,11 @@ request.on('response', function(response) {
 				console.log('parameters: ', parameters);
 				switch(action){
 					case 'account.balance':
-					sendTextMessage(senderID, 'get account balance');
+					checkAccount(senderID, "balance");		//sendTextMessage(senderID, 'get account balance');
 					break;
 					case 'account.movement':
-					sendTextMessage(senderID, 'get account movement');
+					checkAccount(senderID, "balance");	//sendTextMessage(senderID, 'get account movement');
+					
 					break;
 					default:
 					console.log('unknown action...');
@@ -315,7 +316,52 @@ request.on('response', function(response) {
 		}
 	}
 });
+function checkAccount(senderID,messages){
+	const value = encodeURI(message);
+	
+	request({
+    uri: 'https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=508rating=pg&q=',
+   
 
+  }, function (error, response, body) {
+	  
+    if (!error && response.statusCode == 200) {
+		var parsed = JSON.parse(body);
+		var i = Math.floor(Math.random()*10);
+		var meme = parsed.data[i];
+		
+		if(meme && meme.images && meme.fixed_width){
+			var giphy = meme.images.fixed_width;
+			request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json {
+		recipient: {
+			id: senderID
+			
+		
+		message :{
+			attachment:{
+				type: 'image',
+				payload:{
+					url: giphy.url
+				}
+			}
+		}
+		}
+	}, function (error, response, body) {
+		
+	
+    
+  });
+
+    } else {
+      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+    }
+  });
+	
+}
 request.on('error', function(error) {
     console.log(error);
 });
